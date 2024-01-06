53b992e5cff4f9a27dad6442047a357dc93cbd03
ViktoryiaKaranchuk <vika.korenchyk@gmail.com>
2024-01-06 12:34:33 +0100

    project

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,25 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.time.Duration;
+
+import static utils.DriverProvider.getCurrentDriver;
+
+
 public class HomePage {
 
+    String userProfileIconXpath = "//*[@class ='wd3S']";
+
+
+    public void goToLoginPage() {
+        WebElement userProfileIcon = new WebDriverWait(getCurrentDriver(), Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(userProfileIconXpath)));
+        userProfileIcon.click();
+
+    }
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,21 +1,44 @@
 package tests;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.support.ui.ExpectedConditions;
+        import org.openqa.selenium.support.ui.WebDriverWait;
+        import org.testng.annotations.AfterMethod;
+        import org.testng.annotations.BeforeMethod;
 
-import static utils.DriverProvider.closeDriver;
-import static utils.DriverProvider.getCurrentDriver;
+        import java.time.Duration;
+
+        import static utils.DriverProvider.closeDriver;
+        import static utils.DriverProvider.getCurrentDriver;
 
 public class BaseTest {
+    String acceptCookiesButtonId = "onetrust-accept-btn-handler";
 
     @BeforeMethod
     public void setUp() {
-
-        getCurrentDriver().get("https://www.mediaexpert.pl/");
-
+        getCurrentDriver().get("https://www2.hm.com/pl_pl/");
+        maximizeWindow();
+        acceptCookies();
     }
 
     @AfterMethod
-    public void tearDown() {
-        closeDriver();
+    public void tearDown() { closeDriver(); }
+
+    public void maximizeWindow() {
+        getCurrentDriver().manage().window().maximize();
     }
+
+    public void acceptCookies() {
+        WebDriverWait wait = new WebDriverWait(getCurrentDriver(), Duration.ofSeconds(30));
+        By acceptCookiesButtonLocator = By.id(acceptCookiesButtonId);
+
+        try {
+            WebElement acceptCookiesButton = wait.until(ExpectedConditions.elementToBeClickable(acceptCookiesButtonLocator));
+            acceptCookiesButton.click();
+        } catch (Exception e) {
+
+        }
+    }
+
 }
+
