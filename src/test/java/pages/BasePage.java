c1ad72f144edd087dbaf48a096d52bdd6bba273b
gaevskiy94 <gaevskiy.alexandr@gmail.com>
2024-01-09 01:51:44 +0200

    Add second test

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -7,10 +7,41 @@ import static utils.DriverProvider.getCurrentDriver;
 
 public class BasePage {
     String perfumyCatalogLinkXpath = "//a[@href='/pl/menu/perfumy-153.html']";
+    String userMenuIconId = "menu_additional";
+    String loginLinkXpath = "//a[@class='account_link']";
+    String logoutButtonXpath = "//div[@class='menu_additional__links']//a[@class='logout_link']";
 
     public void openPerfumyCatalog(){
         WebElement perfumyCatalogLink = getCurrentDriver().findElement(By.xpath(perfumyCatalogLinkXpath));
         perfumyCatalogLink.click();
+    }
+
+    public void openAuthorizationPage(){
+        WebElement userMenuIcon = getCurrentDriver().findElement(By.id(userMenuIconId));
+        userMenuIcon.click();
+
+        WebElement loginLink = getCurrentDriver().findElement(By.xpath(loginLinkXpath));
+        loginLink.click();
+    }
+
+    public boolean isUserAuthorized(){
+        WebElement userMenuIcon = getCurrentDriver().findElement(By.id(userMenuIconId));
+        userMenuIcon.click();
+
+        try {
+            WebElement logoutButton = getCurrentDriver().findElement(By.xpath(logoutButtonXpath));
+            return logoutButton.isDisplayed();
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public void logout(){
+        WebElement userMenuIcon = getCurrentDriver().findElement(By.id(userMenuIconId));
+        userMenuIcon.click();
+
+        WebElement logoutButton = getCurrentDriver().findElement(By.xpath(logoutButtonXpath));
+        logoutButton.click();
     }
 
     public double getPriceDouble(WebElement element){
