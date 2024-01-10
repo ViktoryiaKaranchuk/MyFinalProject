14245e0068fe980f005db21137cadbb7fc1d0a59
ViktoryiaKaranchuk <vika.korenchyk@gmail.com>
2024-01-10 21:35:56 +0200

    Check git name

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -14,7 +14,7 @@ import static utils.DriverProvider.getCurrentDriver;
 
 public class BaseTest {
     String closeMessageAboutCookiesButtonXpath = "//span[@class='ck_dsclr__btn_v2']";
-String x;
+String xy;
     @BeforeMethod
     public void setUp() {
         getCurrentDriver().get("https://www.ezebra.pl/");
